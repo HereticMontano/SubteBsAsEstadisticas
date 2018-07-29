28c173b1c0a3ace363afba4d1b904d59d272ff4f
Montano <manuelelp@gmail.com>
2018-07-29 01:28:51 -0300

    Primer Commit

---
+++ ConsoleSubteEstadisticas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleSubteEstadisticas
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Process.ProcessRepositories().Wait();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleSubteEstadisticas/SubteStatus.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ConsoleSubteEstadisticas
+{
+    public class SubteStatus
+    {
+        public string Linea { get; set; }
+
+        public SubteDetalle Detalle { get; set; }
+
+    }
+
+    public class SubteDetalle
+    {
+        public string Status { get; set; }
+
+        public string Text { get; set; }
+    }
+}
