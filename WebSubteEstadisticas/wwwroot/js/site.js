0f2c1011d08c116572d26f3922bcbe8c40d7f6a8
Montano <manuelelp@gmail.com>
2018-08-19 20:59:09 -0300

    Mas esqueleto y pruebas

--- WebSubteEstadisticas/wwwroot/js/site.js
+++ WebSubteEstadisticas/wwwroot/js/site.js
@@ -1,12 +1,27 @@
-function NewInstanceChart(id, data) {
+function NewInstanceChart(id, horasSuspendido, horasNormal) {
 
 	var canvas = $("#" + id)[0].getContext('2d');
 	return new Chart(canvas, {
 		type: 'bar',
 		data: {
 			labels: ["Linea A", "Linea B", "Linea C", "Linea D", "Linea E", "Linea H", "Linea P"],
-			datasets: [{				
-				data: data,
+			datasets: [{
+				data: horasSuspendido,
+				label: "Horas suspendido",
+				backgroundColor: [
+					'rgb(5, 173, 222)',
+					'rgb(233, 22, 39)',
+					'rgb(4, 106, 180)',
+					'rgb(8, 127, 105)',
+					'rgb(109, 34, 129)',
+					'rgb(255, 202, 3)',
+					'rgb(253, 186, 45)'
+				],
+				borderWidth: 1
+			},
+			{
+				data: horasNormal,
+				label: "Horas normal",
 				backgroundColor: [
 					'rgb(5, 173, 222)',
 					'rgb(233, 22, 39)',
