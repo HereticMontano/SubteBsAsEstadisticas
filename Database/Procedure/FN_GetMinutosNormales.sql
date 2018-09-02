f68f43da6e28a06a358a39648a24c6bdece41c6d
Montano <manuelelp@gmail.com>
2018-09-02 16:52:32 -0300

    Ahora se agregan al precalculado las lineas que estuvieron funcinado bien todo el dia.
    Se corrige un bug de una situacion que se daba cuando se intetaba restar la diferencia de horas entre una hora de "ayer" 6Hs y otra de "hoy" 00Hs

--- Database/Procedure/FN_GetMinutosNormales.sql
+++ Database/Procedure/FN_GetMinutosNormales.sql
@@ -5,15 +5,23 @@ CREATE OR REPLACE FUNCTION FN_GetMinutosNormales(idLinea TINYINT(4), idFecha INT
 DETERMINISTIC
 BEGIN
 		
+	DROP TEMPORARY TABLE IF EXISTS fechaData;
 	CREATE TEMPORARY TABLE fechaData (Fecha DATE, IdTipoDia TINYINT);
 	INSERT INTO fechaData 
 	SELECT Fecha, IdTipoDia
 	FROM FechaEstadoServicio
 	WHERE Id = idFecha;
 	
-	/*El itinerario del subte a partir de alguna fecha puede ser distinto, por eso obtengo todos los 
-	itinerarios menor a la fecha del estado del servicio  y lo ordeno decreciente para quedarme con el itinerario vigente a la fecha del estado del servicio*/
-	RETURN (SELECT TIMESTAMPDIFF(MINUTE, IT.HoraDesde, IT.HoraHasta) - minutosSuspendido
+	/*
+	El case esta porque si tengo un itinerario que termina a las 00hs como por ejemplo la H el resultado de la diferencia no es el buscado, 
+	asique resto la diferencia a la cantidad de minutos en un dia. 
+	
+	El itinerario del subte a partir de alguna fecha puede ser distinto, por eso obtengo todos los 
+	itinerarios menor a la fecha del estado del servicio  y lo ordeno decreciente para quedarme con el itinerario vigente a la fecha del estado del servicio.
+	*/
+	RETURN (SELECT (CASE WHEN HoraDesde < HoraHasta 
+								THEN TIMESTAMPDIFF(MINUTE,HoraDesde ,HoraHasta) 
+								ELSE 1440 - TIMESTAMPDIFF(MINUTE, HoraHasta, HoraDesde) END) - minutosSuspendido
 	FROM (SELECT  I.* FROM Itinerario I
 			WHERE I.IdLinea = idLinea AND 
 			I.IdTipoDia = (SELECT IdTipoDia FROM fechaData) AND 
--- Database/Procedure/SP_Precalcular.sql
+++ Database/Procedure/SP_Precalcular.sql
@@ -5,12 +5,25 @@ BEGIN
 
 	DELETE FROM Precalculado;
 	
+	/*
+	El case esta porque si tengo una linea que no funciono hasta las 00hs la diferencia no es el dato buscado, 
+	asique resto la diferencia a la cantidad de minutos en un dia. 
+	*/
 	INSERT INTO Precalculado(IdLinea, MinutosSuspendida, IdFecha) 
-	SELECT ES.IdLinea, SUM(TIMESTAMPDIFF(MINUTE, ES.HoraDesde, ES.HoraHasta)), FE.Id		
+	SELECT ES.IdLinea, (CASE WHEN ES.HoraDesde < ES.HoraHasta 
+								THEN TIMESTAMPDIFF(MINUTE, ES.HoraDesde , ES.HoraHasta) 
+								ELSE 1440 - TIMESTAMPDIFF(MINUTE, ES.HoraHasta, ES.HoraDesde) END), FE.Id		
 	FROM FechaEstadoServicio FE 
 	JOIN EstadoServicio ES ON ES.IdFecha = FE.Id 
 	WHERE ES.IdEstado = 2
 	GROUP BY Es.IdLinea;
+	
+	
+	INSERT INTO Precalculado(IdLinea, MinutosSuspendida, IdFecha) 
+	SELECT L.Id, 0, FE.Id	
+	FROM FechaEstadoServicio FE
+	JOIN Precalculado P ON P.idFecha = Fe.id
+   JOIN Linea L ON L.Id <> P.idLinea;
 	
 	UPDATE Precalculado SET MinutosNormal = FN_GetMinutosNormales(IdLinea, IdFecha, MinutosSuspendida);
 
